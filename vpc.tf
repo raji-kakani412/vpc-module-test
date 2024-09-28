ffb7df79a9972c173391f524d559e535b1a1e23f
rajikakani412 <raji.kakani@gmail.com>
2024-09-28 15:12:18 +0530

    updated

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,12 @@
 module "myvpc" {
-  source                = "../Terraform-aws-vpc"
+  source                = "../Terraform-aws-vpc" 
   project_name          = var.project_name
   environment           = var.environment
   common_tags           = var.common_tags
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
-  is_peering_required= true # we can create a variable and provide it also
+  is_peering_required   = true # we can create a variable and provide it also
 }
+
+# source = "git:: https://github.com/raji-kakani412/terraform-aws-vpc.git?ref=main" to pull module from git directly
